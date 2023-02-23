21f85eceb4a12652c0a5c1581da7823bd34ccf87
SUN <sun@SUNui-MacBookPro.local>
2023-02-23 11:33:32 +0900

    [문선정]Assignment 1: Experess API-initial setting(수정사항 반영)

--- seonjeong/app.js
+++ seonjeong/app.js
@@ -1,28 +1,29 @@
 const http = require("http");
+
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-
-const dotenv = require("dotenv");
-dotenv.config()
-
 const { DataSource } = require('typeorm')
 
-const myDataSource = new DataSource({
-    type: process.env.TYPEORM_CONNECTION,
-    host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE
+const app = express();
+
+const dotenv = require("dotenv");
+
+
+const dataSource = new DataSource({
+    type: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 })
 
-myDataSource.initialize()
+dataSource.initialize()
   .then(() => {
     console.log("Data Source has been initialized!")
   });
-
-const app = express()
 
 app.use(express.json());
 app.use(cors());
@@ -30,8 +31,13 @@ app.use(morgan('combined'));
 
 
 app.get("/ping", (req, res) => {
-  res.json({ message : "pong"});
+  res.status(200).json({ message : "pong"});
 });
+
+
+
+
+
 
 
 const server = http.createServer(app)
