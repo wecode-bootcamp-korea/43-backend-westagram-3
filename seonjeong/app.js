e9f604fd184ee6469ea1ef4c7a378257f89ea094
SUN <sun@SUNui-MacBookPro.local>
2023-02-24 21:36:08 +0900

    [문선정] Assignment 2: 유저 회원가입 하기

--- seonjeong/app.js
+++ seonjeong/app.js
@@ -1,43 +1,56 @@
-const http=require("http");
+const http = require("http");
 
-require('dotenv').config();
-const express=require("express");
-const cors=require("cors");
-const morgan=require("morgan");
-const { DataSource }=require('typeorm')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { DataSource } = require("typeorm");
 
-const app=express();
+const app = express();
 
-const dotenv=require("dotenv");
+const dotenv = require("dotenv");
 
-const dataSource=new DataSource({
-    type: process.env.DB_CONNECTION,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
+const dataSource = new DataSource({
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
-dataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!")
-  });
+dataSource.initialize().then(() => {
+  console.log("Data Source has been initialized!");
+});
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
+/*app.get("/ping", (req, res) => {
+  res.status(200).json({ message: "pong" });
+});*/
 
-app.get("/ping", (req, res) => {
-  res.status(200).json({ message : "pong"});
+app.post("/users", async (req, res) => {
+  const { name, email, profileImage, password } = req.body;
+
+  await dataSource.query(
+    `INSERT INTO users(
+      name,
+      email,
+      profile_image,
+      password
+      ) VALUES (?, ?, ?, ?);`,
+    [name, email, profileImage, password]
+  );
+  res.status(201).json({ message: "successfully created" });
 });
 
-const server=http.createServer(app)
-const PORT=process.env.PORT;
+const server = http.createServer(app);
+const PORT = process.env.PORT;
 
 const start = async () => {
   server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
-}
+};
 
-start()
+start();
